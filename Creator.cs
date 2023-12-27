f4d06a2c3dfa480ed5745f043ecbd0e26320da5f
a.amraei <amin.amraei@gmail.com>
2023-12-27 11:40:22 +0330

    Add project files.

---
+++ Creator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+        public string SomeOperation()
+        {            
+            var product = FactoryMethod();
+            // Now, use the product.
+            var result = "Creator: The same creator's code has just worked with "
+                + product.Operation();
+
+            return result;
+        }
+    }
+}
